c3cc6fb6516b99f017991a465783d4eaca5c8a3f
Ayoub_L <ayoublaissaoui.1@gmail.com>
2022-05-31 14:52:01 +0100

    just need to handle the json data and render the required information

--- client/JS/app.js
+++ client/JS/app.js
@@ -11,12 +11,31 @@ function search(){
             'X-RapidAPI-Key': '01e7be827fmshdc10eebfd845863p1f733bjsn67e76cc5fd78'
         }
     };
-    fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
+    fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=luffy&pageNumber=1&pageSize=10&autoCorrect=true', options)
 	.then(response => response.json())
-	.then(response => console.log(response.value))
+	.then(json => {
+        let data1 = "";
+                json.map((values) => {
+                    data1 += `<div class="card">
+                    <h1 class="title">${values.title}</h1>
+                    <img src="${values.image}" alt="img"  class="img-result">
+                    <p>${values.description}</p>
+                    <p class="catergory">${values.url}</p>
+                    <p class="price">${values.title}</p>
+                </div>`
+                })
+                document.getElementById("cards").innerHTML = data1;
+    })
 	.catch(err => console.error(err));
+
 }
+
+
 
 module.exports = {
     search,
 }
+
+
+
+
